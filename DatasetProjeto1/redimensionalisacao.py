ffc75ff3b90bce45746ae737e5ac24267a4ad8fa
sylrod569 <mattheus15bruno@gmail.com>
2026-02-20 19:37:11 -0300

    Adicionando o treino e teste dos três modelores lineares implementados para a classificação dos dígitos 1 e 5

--- DatasetProjeto1/redimensionalisacao.py
+++ DatasetProjeto1/redimensionalisacao.py
@@ -5,30 +5,49 @@ import pandas as pd
 # pega a pasta onde ta o script pra achar os csv
 pasta = Path(__file__).parent
 
-
 # calcula a intensidade: soma todos os pixels e divide por 255
 def calcular_intensidade(img):
-    return img.sum() / 255.0
-
+    pixels = 0
+    
+    for pixelx in range(img.shape[0]):
+        for pixely in range(img.shape[1]):
+            pixels += img[pixelx, pixely]
+    
+    return (pixels / 255.0)
+    #return img.sum() / 255.0
 
 # simetria vertical: compara lado esquerdo com direito da imagem
 def calcular_simetria_vertical(img):
-    esquerda = img[:, :14]
-    direita = img[:, 14:][:, ::-1]
-    sv = np.abs(esquerda - direita).sum() / 255.0
-    return sv
+    sv = 0
+    
+    for i in range(28):
+        for j in range(14):
+            sv += np.abs(img[i][j] - img[i][27-j])
+    
+    return (sv/255.0)
+
+    #esquerda = img[:, :14]
+    #direita = img[:, 14:][:, ::-1]
+    #sv = np.abs(esquerda - direita).sum() / 255.0
+    #return sv6
 
 # simetria horizontal: compara metade de cima com metade de baixo
 def calcular_simetria_horizontal(img):
-    cima = img[:14, :]
-    baixo = img[14:, :][::-1, :]
-    sh = np.abs(cima - baixo).sum() / 255.0
-    return sh
+    sh = 0
+    
+    for i in range(14):
+        for j in range(28):
+            sh += np.abs(img[i][j] - img[27-i][j])
+    
+    return (sh/255.0)
+    #cima = img[:14, :]
+    #baixo = img[14:, :][::-1, :]
+    #sh = np.abs(cima - baixo).sum() / 255.0
+    #return sh
 
 # simetria total = vertical + horizontal
 def calcular_simetria(img):
     return calcular_simetria_vertical(img) + calcular_simetria_horizontal(img)
-
 
 # le o csv, calcula intensidade e simetria pra cada imagem e retorna um dataframe
 def processar(caminho_csv):
@@ -54,7 +73,6 @@ def processar(caminho_csv):
     })
 
     return resultado
-
 
 if __name__ == "__main__":
 
